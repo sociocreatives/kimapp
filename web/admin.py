bfaa3bf0375198a7c7d700880e02a5340fbc4175
ERICK OMONDI AYARA <ayaraerick@gmail.com>
2024-05-11 13:58:44 +0300

    styles

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Legal
+from .models import Faqs
+from .models import ExpertTips
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
+admin.site.register(Legal, ImportExportModelAdmin)
+admin.site.register(Faqs, ImportExportModelAdmin )
+admin.site.register(ExpertTips, ImportExportModelAdmin)
